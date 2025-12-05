9598dadd750c7e572cc5f6b34499555df7eec246
Jason Bedard <jason+github@jbedard.ca>
2025-12-04 20:46:33 -0800

    day4 part2 add todo-queue implementation

--- day4/main.go
+++ day4/main.go
@@ -4,8 +4,48 @@ import "github.com/jbedard/aoc2025/lib"
 
 func main() {
 	g := lib.NewCharGridFromSeq(lib.ReadInputLines())
-	c := 0
+	c := solution2(g)
 
+	println("Count:", c)
+}
+
+func solution2(g lib.Grid[rune]) int {
+	c := 0
+	todo := []lib.Coord{}
+	for y := 0; y < g.H(); y++ {
+		for x := 0; x < g.W(); x++ {
+			todo = append(todo, lib.Coord{X: x, Y: y})
+		}
+	}
+
+	for len(todo) > 0 {
+		now_todo := make([]lib.Coord, 0, len(todo))
+
+		for _, coord := range todo {
+			if canMove(g, coord.X, coord.Y) {
+				g.Set(coord.X, coord.Y, '.')
+				c++
+
+				for dy := -1; dy <= 1; dy++ {
+					for dx := -1; dx <= 1; dx++ {
+						nx := coord.X + dx
+						ny := coord.Y + dy
+						if nx >= 0 && nx < g.W() && ny >= 0 && ny < g.H() && !(dx == 0 && dy == 0) {
+							now_todo = append(now_todo, lib.Coord{X: nx, Y: ny})
+						}
+					}
+				}
+			}
+		}
+
+		todo = now_todo
+	}
+
+	return c
+}
+
+func solution1(g lib.Grid[rune]) int {
+	c := 0
 	for {
 		moved := false
 
@@ -21,8 +61,7 @@ func main() {
 			break
 		}
 	}
-
-	println("Count:", c)
+	return c
 }
 
 func canMove(g lib.Grid[rune], x, y int) bool {
