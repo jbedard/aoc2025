1f441447ce2ea8ec18bc8b3181d050ee1a64a4a8
Jason Bedard <jason+github@jbedard.ca>
2025-12-05 22:52:51 -0800

    day6 part2

--- day6/main.go
+++ day6/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"slices"
 	"strconv"
 	"strings"
 
@@ -34,6 +35,9 @@ func parseInput1() ([][]int, []string) {
 func main() {
 	g, o := parseInput1()
 	println("Part 1:", part1(g, o))
+
+	g, o = parseInput2()
+	println("Part 2:", part1(g, o))
 }
 
 func part1(g [][]int, o []string) int {
@@ -46,6 +50,12 @@ func part1(g [][]int, o []string) int {
 		}
 
 		for _, row := range g {
+			// ignore zero values which may be padding for numbers
+			// of less digits
+			if row[i] == 0 {
+				continue
+			}
+
 			switch op {
 			case "*":
 				v = v * row[i]
@@ -58,4 +68,83 @@ func part1(g [][]int, o []string) int {
 	}
 
 	return t
+}
+
+func parseInput2() ([][]int, []string) {
+	/*
+		123 328  51 64
+		 45 64  387 23
+		  6 98  215 314
+		*   +   *   +
+
+		must transform to:
+
+		g: {
+			{4,   431, 623},
+			{175, 581, 32},
+			{8,   248, 369},
+			{356, 24,  1}
+		}
+		o: "*", "+", "*", "+"
+	*/
+
+	lines := slices.Collect(lib.ReadInputLines())
+
+	ops := []string{}
+	offsets := []int{}
+	for i, char := range lines[len(lines)-1] {
+		if char == '*' || char == '+' {
+			ops = append(ops, string(char))
+			offsets = append(offsets, i)
+		}
+	}
+
+	// Drop the operations line
+	lines = lines[:len(lines)-1]
+
+	// Preallocade grid of chars and ints
+	sgrid := make([][]rune, len(lines))
+
+	// Convert to a grid of chars
+	for r, line := range lines {
+		sgrid[r] = make([]rune, len(line))
+		for c, ch := range line {
+			sgrid[r][c] = ch
+		}
+	}
+
+	/* sgrid [][]rune:
+		123 328  51 64				 1	  369
+		 45 64  387 23			=>	 24   248 ...
+		  6 98  215 314			     356  8
+
+	   o []string: "*", "+", "*", "+"
+	*/
+
+	g := [][]int{}
+	for col, offset := range offsets {
+		opNums := []int{}
+		for colOffset := offset; (col == len(offsets)-1 && colOffset < len(sgrid[0])) || (col < len(offsets)-1 && colOffset < offsets[col+1]-1); colOffset++ {
+			v := 0
+			for _, row := range sgrid {
+				if row[colOffset] != ' ' {
+					v = v*10 + int(row[colOffset]-'0')
+				}
+			}
+
+			opNums = append(opNums, v)
+		}
+
+		for c, n := range opNums {
+			for len(g) <= c {
+				g = append(g, make([]int, len(offsets)))
+			}
+			if g[c][col] != 0 {
+				panic("unexpected non-zero value")
+			}
+			g[c][col] = n
+		}
+	}
+
+	return g, ops
 }
