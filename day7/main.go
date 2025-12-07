73b3f6ae38f2271b07c70ff52246185a9c0558f4
Jason Bedard <jason+github@jbedard.ca>
2025-12-06 21:19:47 -0800

    day7 part2

--- day7/main.go
+++ day7/main.go
@@ -5,30 +5,38 @@ import "github.com/jbedard/aoc2025/lib"
 func main() {
 	g := lib.NewCharGridFromSeq(lib.ReadInputLines())
 
-	println("Part 1:", part1(g))
+	count, forks := part1(g)
+	println("Part 1:", count)
+	println("Part 2:", forks)
 }
 
-func part1(g lib.Grid[rune]) int {
-	beams := make([]bool, g.W())
+func part1(g lib.Grid[rune]) (int, int) {
+	beams := make([]int, g.W())
 	splits := 0
 	for _, row := range g.Rows() {
 		for x, cell := range row {
 			switch cell {
 			case 'S':
-				beams[x] = true
+				beams[x]++
 			case '^':
-				if beams[x] {
-					beams[x] = false
+				if c := beams[x]; c != 0 {
+					beams[x] = 0
 					if 0 < x {
-						beams[x-1] = true
+						beams[x-1] += c
 					}
-					if x < g.W()-1 && !beams[x+1] {
-						beams[x+1] = true
+					if x < g.W()-1 {
+						beams[x+1] += c
 					}
 					splits++
 				}
 			}
 		}
 	}
-	return splits
+
+	beamCount := 0
+	for _, b := range beams {
+		beamCount += b
+	}
+
+	return splits, beamCount
 }
