3e7166d6f9f13e2afb6fd12efcfdec9687287506
Jason Bedard <jason+github@jbedard.ca>
2025-12-09 15:20:06 -0800

    day9: add tests

--- day9/day9_test.go
+++ day9/day9_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jbedard/aoc2025/lib"
 )
 
+// Practice problem test case
 const TEST_INPUT = `7,1
 11,1
 11,7
@@ -31,5 +32,50 @@ func TestPart1(t *testing.T) {
 	r2 := part2_b(points)
 	if r2 != 24 {
 		t.Errorf("expected 24, got %d", r2)
+	}
+}
+
+// Custom test cases
+const TEST_INPUT_2 = `7,1
+11,1
+11,3
+13,3
+13,5
+11,5
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3
+`
+
+const TEST_INPUT_3 = `
+1,2
+8,2
+8,4
+6,4
+6,5
+9,5
+9,8
+1,8
+`
+
+var TESTS = map[string]int{
+	TEST_INPUT_2: 36,
+	TEST_INPUT_3: 56,
+}
+
+func Test2(t *testing.T) {
+	for input, expected := range TESTS {
+		points := []lib.Pos2d{}
+		for line := range lib.ReadLines(strings.TrimSpace(input)) {
+			points = append(points, lib.ReadPos2d(line))
+		}
+
+		r2 := part2_b(points)
+		if r2 != expected {
+			t.Errorf("expected %d, got %d", expected, r2)
+		}
 	}
 }
