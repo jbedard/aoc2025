2e27bdd476dffacfa57d7f15dd9788cbbaea3ac3
Jason Bedard <jason+github@jbedard.ca>
2025-12-11 20:40:29 -0800

    progress bar: rounding

--- lib/term.go
+++ lib/term.go
@@ -8,7 +8,7 @@ import (
 func Progress(t time.Time, m string, args ...interface{}) {
 	fmt.Print("\x1b7")     // save the cursor position
 	fmt.Print("\x1b[2K\r") // erase the current line
-	fmt.Printf("\x1B[33m %s: %s\x1B[0m", time.Since(t), fmt.Sprintf(m, args...))
+	fmt.Printf("\x1B[33m %f: %s\x1B[0m", time.Since(t).Seconds(), fmt.Sprintf(m, args...))
 	fmt.Print("\x1b8") // restore the cursor position
 }
 func ProgressDone() {
